ce7d5ed5ace9acf783d1928556c7e5f542c95e6e
Uberdanalmeida <105062084+Uberdanalmeida@users.noreply.github.com>
2025-11-26 18:43:44 -0300

    Criando o useState.

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,17 @@ import Header from "./Components/Header";
 import Main from "./Components/Main";
 import Aside from "./Components/Aside";
 import Cadastro from './Components/Cadastro';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [estoque, setEstoque] = useState([{
+    produto: 'qualquer coisa',
+    categoria: '1212',
+    quantidade: 10,
+    preço: 10,
+  }])
+
   return(
     <div className="principal">
       <Header></Header>
