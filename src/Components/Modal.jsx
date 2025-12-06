d2997248b54aada7b07a147b42a23931fad36d29
Uberdanalmeida <105062084+Uberdanalmeida@users.noreply.github.com>
2025-12-05 23:46:09 -0300

    Finalizando a criação, e a estilização do formulário.

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -2,22 +2,32 @@ export default function Modal() {
     return(
         <div className="novoProduto">
             <form action="" method="get">
-            <h2>Novo Produto</h2>
-            <p>Nome do Produto *</p>
-            <input type="text" name="" id="" placeholder="Ex: Notebook Dell"/>
-            <p>Descrição</p>
-            <input type="text" name="" id="" placeholder="Descrição do Produto"/>
-            <div>
-                <p>Quantidade *</p>
-                <input type="number" name="" id="" placeholder="0"/>
-                <p>Preço(R$) *</p>
-                <input type="number" name="" id="" placeholder="0,00"/>
+
+            <h2>Novo Produto *</h2>
+
+            <span>Nome do Produto *
+            <p><input type="text" name="" id="ExInput" placeholder="Ex: Notebook Dell"/></p>
+            </span>
+
+            <span>Descrição
+            <p><input type="text" name="" id="ExInput" placeholder="Descrição do Produto"/></p>
+            </span>
+
+            <div className="quantidadePreco">
+                <span className="quanPre">Quantidade *
+                <p><input type="number" name="" id="ExInput" placeholder="0"/></p></span>
+
+                <span className="quanPre">Preço(R$) *
+                <p><input type="number" name="" id="ExInput" placeholder="0,00"/></p></span>
             </div>
-            <p>Categoria</p>
-            <input type="text" name="" id="" placeholder="Ex: Eletrônicos"/>
-            <div>
-                <button>Cancelar</button>
-                <button>Adicionar</button>
+
+            <span>Categoria
+            <p><input type="text" name="" id="ExInput" placeholder="Ex: Eletrônicos"/></p>
+            </span>
+
+            <div className="CancelaAdicionar">
+                <input className="AdiCancel" type="button" value="Cancelar" />
+                <input className="AdiCancel" type="button" value="Adicionar" />
             </div>
             </form>
         </div>
